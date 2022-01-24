12d8853273514cc687689621f52f515a56077a60
sojiro14 <taishi.hiraga+github@gmail.com>
2022-01-24 22:52:38 +0900

    feat: add config and favicon

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // site config
+  lang: 'ja-JP',
+  title: "Sojiro's Blog",
+  description: 'Everything written here is a xxx',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
+
+  // theme and its config
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: '/favicon.png',
+  },
+  
+}
