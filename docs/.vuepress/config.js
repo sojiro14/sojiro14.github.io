d8c43a9d6a406246eb291c9d0f99febcfc766504
sojiro14 <taishi.hiraga+github@gmail.com>
2022-01-25 00:38:12 +0900

    feat: add search

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   // site config
   lang: 'ja-JP',
   title: "Sojiro's Blog",
-  description: 'Everything written here is a xxx',
+  description: 'Everything written here is a xxx.',
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }]
   ],
@@ -11,6 +11,11 @@ module.exports = {
   theme: '@vuepress/theme-default',
   themeConfig: {
     logo: '/favicon.png',
+    navbar: [
+      { text: 'home', link: '/' },
+      { text: 'archive', link: '/archive/' },
+      { text: 'about', link: '/about/' }
+    ],
   },
   
   // plugins
@@ -19,6 +24,22 @@ module.exports = {
       '@vuepress/plugin-google-analytics',
       {
         id: 'G-ECN7E21ZM0',
+      },
+    ],
+    [
+      '@vuepress/plugin-search',
+      {
+        locales: {
+          '/': {
+            placeholder: 'Search',
+          },
+          '/ja/': {
+            placeholder: '検索',
+          },
+          '/zh/': {
+            placeholder: '搜索',
+          },
+        },
       },
     ],
   ],
